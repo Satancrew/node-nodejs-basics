82f890a11a2cb8689b67c46c74313e28e8bd16e5
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-09 18:26:57 +0300

    feat: add args.js task solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let answer = '';
+  for (let i = 0; i < args.length; i += 2) {
+    const argsName = args[i].replace('--', '');
+    const value = args[i + 1];
+    answer += `${argsName} is ${value}, `;
+  }
+  console.log(answer);
 };
 
 parseArgs();
