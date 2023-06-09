2bf0529c58c7c6584d6f02473edb93a2e2a4eadd
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-09 16:23:21 +0300

    feat: add rename.js task solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(filename);
+
 const rename = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const pathToRenamedFile = path.join(__dirname, 'files', 'properFilename.md');
+  try {
+    await fs.promises.rename(pathToFile, pathToRenamedFile);
+    console.log('File renamed successfully.');
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
