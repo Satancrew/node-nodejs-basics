1a589aad5f007c643c5feb6718cbb1ad6a189e4f
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-10 13:58:04 +0300

    feat: add calcHash.js task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import crypto from 'crypto';
+
+const filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const pathToFile = path.join(
+      __dirname,
+      'files',
+      'fileToCalculateHashFor.txt',
+    );
+    const data = await fs.promises.readFile(pathToFile);
+    const hash = crypto.createHash('sha256');
+
+    hash.update(data);
+    const answer = hash.digest('hex');
+    
+    console.log(answer);
+  } catch (error) {
+    throw new Error('You got some error');
+  }
 };
 
 await calculateHash();
