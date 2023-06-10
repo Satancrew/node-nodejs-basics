71213ac6ae79e63f9204229445bf634d341ba8fe
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-10 13:15:09 +0300

    feat: add streams/transform.js task solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,11 +1,15 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-
-const filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(filename);
+import { Transform } from 'stream';
 
 const transform = async () => {
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reverseText = chunk.toString().split('').reverse().join('');
+      this.push(reverseText);
+      callback();
+    },
+  });
+  process.stdin.pipe(reverseStream);
+  reverseStream.pipe(process.stdout);
 };
 
 await transform();
