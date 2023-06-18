02428a4f9890b945bb5e782663e8077b28a7485d
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-18 17:54:14 +0300

    feat: add modules/esm.mjs task solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,8 +6,8 @@ import { spawn } from 'child_process';
 const filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(filename);
 const pathToFile = path.join(__dirname, 'files', 'script.js');
-
 const spawnChildProcess = async args => {
+
   const child = spawn('node', [pathToFile, ...args]);
   process.stdin.pipe(child.stdin);
   child.stdout.pipe(process.stdout);
